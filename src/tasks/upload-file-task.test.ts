ee9e32c6d64c8e100098dc9ed4a1fb51f13dd0ab
Kim Lan Phan Hoang <pyphilia@gmail.com>
2022-06-21 14:10:23 +0200

    refactor: throw custom error for empty files (#25)

--- src/tasks/upload-file-task.test.ts
+++ src/tasks/upload-file-task.test.ts
@@ -9,12 +9,11 @@ import {
   buildDefaultLocalOptions,
   FILE_SERVICES,
   DEFAULT_S3_OPTIONS,
-  IMAGE_PATH,
   TEXT_FILE_PATH,
 } from '../../test/fixtures';
 import { LocalService } from '../fileServices/localService';
 import { S3Service } from '../fileServices/s3Service';
-import { UploadFileInvalidParameterError } from '../utils/errors';
+import { UploadEmptyFileError, UploadFileInvalidParameterError } from '../utils/errors';
 import UploadFileTask from './upload-file-task';
 
 const handler = {} as unknown as DatabaseTransactionHandler;
@@ -91,7 +90,7 @@ describe('Upload File Task', () => {
 
     const task = new UploadFileTask(actor, buildFileService(service), input);
     expect(async () => await task.run(handler, log)).rejects.toEqual(
-      new UploadFileInvalidParameterError({
+      new UploadEmptyFileError({
         file: DEFAULT_FILE_STREAM,
         filepath: input.filepath,
         size: input.size,
