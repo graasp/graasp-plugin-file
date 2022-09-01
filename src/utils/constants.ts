b820457e641aeb7ba012809f893d803a4b95ec05
Alvaro Bautista <67112345+alvrba@users.noreply.github.com>
2022-09-01 18:10:42 +0200

    chore: change s3 presigned url expiration time

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,7 @@ export const ORIGINAL = 'original';
 
 export const THUMBNAIL_SIZES = [SMALL, MEDIUM, LARGE, ORIGINAL];
 
-export const S3_PRESIGNED_EXPIRATION = 600; // default 60s
+export const S3_PRESIGNED_EXPIRATION = 3600; // default 600s
 export const MAX_NUMBER_OF_FILES_UPLOAD = 20;
 export const MAX_NB_TASKS_IN_PARALLEL = 5;
 
